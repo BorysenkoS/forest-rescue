b292762829d5d36e78aee76dbc582d82fe0a6f27
Serhii Borysenko <borisenko37@gmail.com>
2024-11-04 15:42:13 +0100

    add reviews section for mob

---
+++ src/js/reviews.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper-container', {
+  direction: 'vertical',
+  loop: true,
+  slidesPerView: 3,
+  speed: 800,
+  navigation: {
+    nextEl: '.button-next',
+  },
+});
